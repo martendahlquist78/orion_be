346cc00542e19e492a1bfe6c8f849008a0e08c01
Mårten Dahlquist <marten@dahlquist.info>
2016-01-20 08:49:09 +0100

    Full rebuild

---
+++ OrionBE/src/main/java/info/dahlquist/app/App.java
@@ -0,0 +1,14 @@
+package info.dahlquist.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+ 
